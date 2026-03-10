f5981bb05e15bc550257e520064b3e2dc0b1c38b
karma <captainav0608@gmail.com>
2026-03-10 05:42:34 +0530

    [feat] add essential dev package to dev.nix Signed-off-by: karma <captainav0608@gmail.com>
    Signed-off-by: karma <captainav0608@gmail.com>
    
    Signed-off-by: karma <captainav0608@gmail.com>

--- config/modules/dev.nix
+++ config/modules/dev.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
+    git
+    lazygit   # TUI for git (makes staging/rebasing 10x faster)
     go
     docker
     docker-compose
     opencode
     nil
+    neofetch
+    eza                  # Modern 'ls' (matches your Starship/Alacritty vibe)
+    ripgrep              # Blazing fast 'grep' replacement
+    bat                  # 'cat' with syntax highlighting
+    bottom               # A much cooler, graphical 'top' (process monitor)
+    curlie               # curl but with a much better UI (like HTTPie)
+    jq
   ];
 }
